3f3c3101556d1eba85e298b5edf843d667c827e6
Peter Grlický <peter.grlicky@sharpwave.dev>
2022-08-27 11:37:55 +0200

    rpi fix

--- init.sh
+++ init.sh
@@ -3,6 +3,8 @@ set -e
 
 source ./common.sh
 
+EXTRA_ARGS="$EXTRA_ARGS -r $RESTIC_REPOSITORY" # weird behaviour of restic on my rpi needed to explicitly use -r parameter
+
 echo "Initializing restic repository $RESTIC_REPOSITORY"
 restic $EXTRA_ARGS init
 printf "Repository initialized!\n\n"
