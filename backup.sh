0cb73723d4cf144763b812edf7132416ecac728d
Peter Grlický <peter.grlicky@sharpwave.dev>
2022-08-27 12:56:55 +0200

    logs

--- backup.sh
+++ backup.sh
@@ -12,18 +12,22 @@ else
 	EXCLUDE=$EXCLUDE,$DEFAULT_EXCLUDES
 fi
 
+echo "Starting backup"
 restic backup \
 	--one-file-system \
 	--exclude-caches \
 	--exclude={$EXCLUDE} \
 	$EXTRA_ARGS / > "/tmp/backup_$CURRENT_DATE" 2>&1
+echo "Finished backup"
 
+echo "Generating diff"
 restic snapshots $EXTRA_ARGS \
 	| tail -n 4 \
 	| head -n 2 \
 	| cut -f 1 -d ' ' \
 	| xargs restic diff $EXTRA_ARGS >> "/tmp/backup_$CURRENT_DATE" 2>&1
 
+echo "Sending report"
 mail -s "Backup Report Workstation $CURRENT_DATE" $REPORT_MAIL < "/tmp/backup_$CURRENT_DATE"
 
 # add webhook for storing the file and sending notification with link to it?
